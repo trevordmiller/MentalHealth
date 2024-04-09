5f8f06d88fe2b6967e821e3030fd9c3d0acf2079
Trevor Miller <5497885+trevordmiller@users.noreply.github.com>
2024-04-09 12:22:29 -0600

    Remove sidebar width override in favor of defaults

--- MentalHealth/Questionnaires/QuestionnairesView.swift
+++ MentalHealth/Questionnaires/QuestionnairesView.swift
@@ -5,9 +5,6 @@ struct QuestionnairesView: View {
     var body: some View {
         NavigationSplitView {
             QuestionnairesListView()
-#if os(macOS)
-                .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-#endif
         } detail: {
             Text("Select A Questionnaire")
         }
